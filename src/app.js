130a281ad5cf4ce3096b8dd353f82577f6e64837
Kneeyuh <niacarey02@gmail.com>
2024-05-01 20:18:13 -0400

    Added Api to reflect the current temp

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#temp");
+  let temperature = Math.round(response.data.temperature.current);
+
+  temperatureElement.innerHTML = temperature;
+}
+
+function SearchCity(city) {
+  let apiKey = "633bc35a46ofab649caf20e3932017t1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
 function handleSubmit(event) {
-    event.preventDefault();
-let searchInput = document.querySelector("#search-input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+  SearchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
